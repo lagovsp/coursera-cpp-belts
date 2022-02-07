cf8219815ab8d030f5a33711c22ce7cd5352fe18
justddreamm <lagovsp@gmail.com>
2022-02-07 18:31:17 +0300

    OPTIMIZED, local tests pass, seems good

--- week-2/ebook.cpp
+++ week-2/ebook.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 
 #include "test_runner.h"
-#include "profiler.h"
 
 using namespace std;
 
@@ -12,24 +11,24 @@ class ReadingManager {
  public:
   ReadingManager() : pages_by_user(USERS_LIMIT + 1, 0),
 					 users_by_page(PAGES_LIMIT + 1, 0),
-					 users(0),
+					 rating(PAGES_LIMIT + 1, 1),
 					 max_page(0),
-					 rating(PAGES_LIMIT + 1, 1) {}
+					 users(0) {}
 
   void Read(const size_t user_id, const size_t page) {
 	max_page = max(max_page, page);
-	size_t prev_page = 0, max_p = max_page;
+	size_t lower = 0, upper = max_page;
 	if (pages_by_user[user_id] == 0) {
 	  ++users;
 	} else {
-	  prev_page = pages_by_user[user_id];
-	  max_p = page;
+	  lower = pages_by_user[user_id];
+	  upper = page;
 	  --users_by_page[pages_by_user[user_id]];
 	}
 	pages_by_user[user_id] = page;
 	++users_by_page[page];
 	if (users > 1) {
-	  Adjust(prev_page, max_p);
+	  Adjust(lower, upper);
 	}
   }
 
@@ -53,25 +52,12 @@ class ReadingManager {
   vector<double> rating;
 
   void Adjust(const size_t page, const size_t page_up) {
+	auto beg = users_by_page.cbegin();
+	size_t others = accumulate(beg, beg + static_cast<int>(page_up + 1), 0);
 	for (size_t i = page_up; i > page; --i) {
-	  size_t others = 0;
-	  for (size_t it = 0; it < i; ++it) {
-		others += users_by_page[it];
-	  }
-//	  cout << "rating p"<<i<<" = " << others << " / " << users - 1 << endl;
-	  rating[i] =
-		  static_cast<double>(others) / static_cast<double>(users - 1);
+	  others -= users_by_page[i];
+	  rating[i] = static_cast<double>(others) / static_cast<double>(users - 1);
 	}
-//	cout << "pages with rating: ";
-//	for (size_t it = 0; it < 15; ++it) {
-//	  cout << rating[it] << " ";
-//	}
-//	cout << endl;
-//	cout << "users by pages: ";
-//	for (size_t it = 0; it < 15; ++it) {
-//	  cout << "p" << it << ": " << users_by_page[it] << " ";
-//	}
-//	cout << endl;
   }
 };
 
@@ -79,59 +65,61 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 
-//  ReadingManager manager;
-//  manager.Read(1, 1);
-//  manager.Read(1, 3);
-//  manager.Read(2, 2);
-//  cout << manager.Cheer(1) << endl;
-//  ASSERT_EQUAL(manager.Cheer(1), 1.0);
-//  ASSERT_EQUAL(manager.Cheer(2), 0.0);
-//  {// reverse order
-//	manager.Read(4, 5);
-//	manager.Read(3, 4);
-//	manager.Read(2, 3);
-//	manager.Read(1, 2);
-//	ASSERT_EQUAL(manager.Cheer(3), 2.0 / 3.0);
-//  }
+  {
+	ReadingManager manager;
+	manager.Read(1, 1);
+	manager.Read(1, 3);
+	manager.Read(2, 2);
+	ASSERT_EQUAL(manager.Cheer(1), 1.0);
+	ASSERT_EQUAL(manager.Cheer(2), 0.0);
+  }
+  {
+	ReadingManager manager;
+	manager.Read(4, 5);
+	manager.Read(3, 4);
+	manager.Read(2, 3);
+	manager.Read(1, 2);
+	ASSERT_EQUAL(manager.Cheer(3), 2.0 / 3.0);
+  }
+  {
+	ReadingManager manager;
+	manager.Read(4, 5);
+	manager.Read(3, 4);
+	manager.Read(4, 6);
+	manager.Read(3, 5);
+	ASSERT_EQUAL(manager.Cheer(4), 1.0);
+	ASSERT_EQUAL(manager.Cheer(3), 0.0);
+	manager.Read(10, 2);
+	ASSERT_EQUAL(manager.Cheer(3), 0.5);
+	ASSERT_EQUAL(manager.Cheer(4), 1.0);
+  }
+  {
+	ReadingManager manager;
+	manager.Read(1, 1);
+	manager.Read(2, 2);
+	manager.Read(3, 3);
+	ASSERT_EQUAL(manager.Cheer(2), 0.5);
+	manager.Read(4, 4);
+	ASSERT_EQUAL(manager.Cheer(3), 2.0 / 3.0);
+  }
+  {
+	ReadingManager manager;
+	int query_count;
+	cin >> query_count;
 
-//  { /*two users read two times*/
-//	ReadingManager manager;
-//	manager.Read(4, 5);
-//	manager.Read(3, 4);
-//	manager.Read(4, 6);
-//	manager.Read(3, 5);
-//	ASSERT_EQUAL(manager.Cheer(4), 1.0);
-//	ASSERT_EQUAL(manager.Cheer(3), 0.0);
-//	manager.Read(10, 2);
-//	ASSERT_EQUAL(manager.Cheer(3), 0.5);
-//	ASSERT_EQUAL(manager.Cheer(4), 1.0);
-//	cout << "tests good" << endl;
-//  }
+	for (int query_id = 0; query_id < query_count; ++query_id) {
+	  string query_type;
+	  cin >> query_type;
+	  int user_id;
+	  cin >> user_id;
 
-//  ReadingManager manager;
-//  manager.Read(1, 1);
-//  manager.Read(2, 2);
-//  manager.Read(3, 3);
-//  ASSERT_EQUAL(manager.Cheer(2), 0.5);
-//  manager.Read(4, 4);
-//  ASSERT_EQUAL(manager.Cheer(3), 2.0 / 3.0);
-
-  ReadingManager manager;
-  int query_count;
-  cin >> query_count;
-
-  for (int query_id = 0; query_id < query_count; ++query_id) {
-	string query_type;
-	cin >> query_type;
-	int user_id;
-	cin >> user_id;
-
-	if (query_type == "READ") {
-	  int page_count;
-	  cin >> page_count;
-	  manager.Read(user_id, page_count);
-	} else if (query_type == "CHEER") {
-	  cout << setprecision(6) << manager.Cheer(user_id) << "\n";
+	  if (query_type == "READ") {
+		int page_count;
+		cin >> page_count;
+		manager.Read(user_id, page_count);
+	  } else if (query_type == "CHEER") {
+		cout << setprecision(6) << manager.Cheer(user_id) << "\n";
+	  }
 	}
   }
 

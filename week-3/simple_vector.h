d6d174afb8b46449ef6704762e40b3ef86a7ea5a
justddreamm <lagovsp@gmail.com>
2022-02-08 04:56:19 +0300

    super mega roflo vector hehe

---
+++ week-3/simple_vector.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <cstdlib>
+#include <algorithm>
+
+template<typename T>
+class SimpleVector {
+ public:
+  SimpleVector() : data_(nullptr), end_(nullptr), size_(0), capacity_(0) {}
+  explicit SimpleVector(const size_t num) {
+	data_ = new T[num];
+	size_ = num;
+	capacity_ = num;
+	end_ = data_ + num;
+  }
+
+  ~SimpleVector() {
+	if (capacity_ == 0) {
+	  return;
+	}
+	delete[] data_;
+  }
+
+  T &operator[](const size_t index) {
+	return data_[index];
+  }
+
+  T *begin() { return data_; }
+  T *end() { return end_; }
+  const T *begin() const { return data_; }
+  const T *end() const { return end_; }
+
+  [[nodiscard]] size_t Size() const {
+	return size_;
+  }
+
+  [[nodiscard]] size_t Capacity() const {
+	return capacity_;
+  }
+
+  void PushBack(const T &value) {
+	if (size_ == 0) {
+	  data_ = new T[++capacity_];
+	} else if (size_ == capacity_) {
+	  T *old = data_;
+	  capacity_ *= 2;
+	  data_ = new T[capacity_];
+	  for (size_t i = 0; i < Size(); ++i) {
+		data_[i] = old[i];
+	  }
+	  delete[] old;
+	}
+	data_[size_++] = value;
+	end_ = data_ + size_;
+  }
+
+ private:
+  T *data_;
+  T *end_;
+  size_t size_;
+  size_t capacity_;
+};

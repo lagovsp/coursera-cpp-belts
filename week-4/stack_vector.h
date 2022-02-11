a77aa5c92afd40cc93698f56c51cba9276011dbb
justddreamm <lagovsp@gmail.com>
2022-02-12 01:24:46 +0300

    StackVector SOLVED + tests PASS

---
+++ week-4/stack_vector.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <stdexcept>
+#include <array>
+
+using namespace std;
+
+template<typename T, size_t N>
+class StackVector {
+ public:
+  explicit StackVector(const size_t size = 0) {
+	if (size > N) {
+	  throw invalid_argument("StackVector::StackVector: size > N");
+	}
+	size_ = size;
+  }
+
+  T &operator[](const size_t index) {
+	return array_[index];
+  }
+
+  const T &operator[](const size_t index) const {
+	return array_[index];
+  }
+
+  auto begin() { return (size_ == 0) ? array_.end() : array_.begin(); }
+  auto end() { return array_.end(); }
+
+  auto begin() const { return (size_ == 0) ? array_.end() : array_.begin(); }
+  auto end() const { return array_.end(); }
+
+  [[nodiscard]] size_t Size() const {
+	return size_;
+  }
+  [[nodiscard]] size_t Capacity() const {
+	return N;
+  }
+
+  void PushBack(const T &value) {
+	if (size_ == N) {
+	  throw overflow_error("StackVector::PushBack: no more space");
+	}
+	array_[size_] = value;
+	++size_;
+  }
+
+  T PopBack() {
+	if (size_ == 0) {
+	  throw underflow_error("StackVector::PopBack: it is empty");
+	}
+	return array_[--size_];
+  }
+
+ private:
+  array<T, N> array_ = {};
+  size_t size_ = 0;
+};
+
